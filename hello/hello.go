a5926c4079484d183c99fc0a68d1ab55deac1674
Binson Zhang <bin183cs@gmail.com>
2013-05-16 09:22:01 +0800

    Add hello/, string/.

---
+++ hello/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+  "fmt"
+  "github.com/bin3/learngo/string"
+)
+
+func main() {
+  fmt.Println("Hello, new gopher!")
+  fmt.Println(string.Reverse("Hello, new gopher!"))
+}
